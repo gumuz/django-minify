1c901e318e388f78e372da877dd90ee5a4529cba
Tommaso Barbugli <tbarbugli@gmail.com>
2013-02-19 17:09:22 +0100

    Update django_minify/__init__.py

--- django_minify/__init__.py
+++ django_minify/__init__.py
@@ -1,11 +1,10 @@
-
 __author__ = 'Rick van Hattem'
 __copyright__ = 'Copyright 2012, Rick van Hattem'
 __credits__ = ['Rick van Hattem']
 
 
 __license__ = 'BSD'
-__version__ = '1.5.93'
+__version__ = '1.5.94'
 __maintainer__ = 'Rick van Hattem'
 __email__ = 'rick@fashiolista.com'
 __status__ = 'Production'
